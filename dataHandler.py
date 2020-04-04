c999b05983481efb8a0192597c75a5b5348cd223
theCVDH <c.cooly1111@web.de>
2020-04-04 18:58:39 +0200

    add user ID

--- dataHandler.py
+++ dataHandler.py
@@ -1,13 +1,9 @@
 import mysql.connector
 sql = mysql.connector.connect(host='localhost',database='infoplattform',user='python',password='salami14')
-mycursor = sql.cursor()
-mycursor.execute("SELECT text FROM test")
-myresult = mycursor.fetchall()
-for x in myresult:
-  print(x)
 
 class User:
-    def __init__(self, email, firstname, lastname, username , advanced = False):
+    def __init__(self,id, email, firstname, lastname, username , advanced = False):
+        self.id
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
