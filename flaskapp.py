68cf8a3f2be6d24717f94ee95330664199851996
The-Northchild <63128696+The-Northchild@users.noreply.github.com>
2020-04-04 13:45:18 +0200

    title added

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+
+def headline():
+    return "<title>Digitale Jugendarbeit<\title>"
+
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
