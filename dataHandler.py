f9785cd6d623ac01dda4317cbcb51b87133880d1
theCVDH <c.cooly1111@web.de>
2020-04-04 22:04:00 +0200

    add user Store

--- dataHandler.py
+++ dataHandler.py
@@ -1,7 +1,16 @@
 import mysql.connector
 sql = mysql.connector.connect(host='localhost',database='infoplattform',user='python',password='salami14')
 
-def loadUser:
+sessions = {}
+def loadSession(SessionId):
+    if SessionId in sessions:
+        return sessions[SessionId]
+    else:
+        return -1
+
+
+
+def loadUser(id):
     pass
 
 class User:
@@ -9,8 +18,9 @@ class User:
     Object represent a User
     """
     insertQuery = "INSERT INTO User (email, firstname, lastname, username, password) VALUES (%s,%s,%s,%s,SHA1(%s))"
-    updateQuery = "INSERT INTO User (email, firstname, lastname, username, password) VALUES (%s,%s,%s,%s,SHA1(%s))"
-    insertQuery = "INSERT INTO User (email, firstname, lastname, username, password) VALUES (%s,%s,%s,%s,SHA1(%s))"
+    updateQuery = "UPDATE User SET email = %s, firstname = %s, lastname = %s, username = %s WHERE id = %s)"
+    deleteQuery = "DELETE FROM User WHERE id = %s)"
+    updatePasswordQuery = "UPDATE User SET email = %s, firstname = %s, lastname = %s, username = %s ,password = %s WHERE id = %s)"
 
     def __init__(self,id, email, firstname, lastname, username, advanced = False):
         """
@@ -29,6 +39,14 @@ class User:
         self.email = email
         self.advanced = advanced
 
+    def delete(self):
+        """
+        delete Entry in database with given id
+        :return:
+        """
+        cursor = sql.getCursor()
+        cursor.execute(self.deleteQuery, (self.id,))
+        sql.commit()
     def store(self , password):
         """
         stores the dataset to database
@@ -39,7 +57,15 @@ class User:
         """
         if self.id == -1 and None == password:
             return False
+        cursor = sql.getCursor()
+        if self.id == -1:
+            cursor.execute(self.insertQuery,(self.email,self.firstname,self.lastname,self.username, password))
 
-
-
-
+        elif password:
+            cursor.execute(self.updatePasswordQuery,(self.email,self.firstname,self.lastname,self.username, password,self.id))
+        else:
+            cursor.execute(self.updateQuery,(self.email,self.firstname,self.lastname,self.username ,self.id))
+        sql.commit()
+        if cursor.rowcount == 0:
+            return False
+        self.id = cursor.lastrowid
