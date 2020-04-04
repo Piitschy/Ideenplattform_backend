9719c316a57d4c0fa262238b28509b960f7da211
Piitschy <jan-d.pietsch@live.de>
2020-04-04 16:42:04 +0000

    some tests with sql

--- sqltest.py
+++ sqltest.py
@@ -1,2 +1,7 @@
 import mysql.connector
-mysql.connector.connect(host='localhost',database='infoplattform',user='python',password='salami14');
+sql = mysql.connector.connect(host='localhost',database='infoplattform',user='python',password='salami14')
+mycursor = sql.cursor()
+mycursor.execute("SELECT text FROM test")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
