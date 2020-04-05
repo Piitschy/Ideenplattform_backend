d94dc61edd0b1f40464bfdee6100e0735237ea03
theCVDH <c.cooly1111@web.de>
2020-04-05 13:07:59 +0200

    Fix change getCursor is now cursor

--- dataHandler.py
+++ dataHandler.py
@@ -50,7 +50,7 @@ class User:
     Object represent a User
     """
     insertQuery = "INSERT INTO User (email, firstname, lastname, username, password) VALUES (%s,%s,%s,%s, SHA1(%s) )"
-    updateQuery = "UPDATE User SET email = %s, firstname = %s, lastname = %s, username = %s WHERE id = %s)"
+    updateQuery = "UPDATE User SET email = %s, firstname = %s, lastname = %s, username = %s WHERE id = %s"
     deleteQuery = "DELETE FROM User WHERE id = %s)"
     updatePasswordQuery = "UPDATE User SET email = %s, firstname = %s, lastname = %s, username = %s ,password = %s WHERE id = %s)"
 
@@ -64,6 +64,7 @@ class User:
         :param username: users username
         :param advanced: True if advanced permission active
         """
+        print( id, "is of type", type(id) )
         self.id = id
         self.firstname = firstname
         self.lastname = lastname
